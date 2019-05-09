b3ee567875c22a118dc723502f4c00a0b5ac5f66
juanmana <46864823+juanmana@users.noreply.github.com>
2019-05-09 16:34:28 +0200

    Login (#1)
    * signup-login backend
    
    * login
    
    * signup hecho
    
    * login
    
    * login signup acabado

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+import AuthService from './components/auth/AuthService';
+import Contents from './components/contents/Contents'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  getUser = (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
+  logout = () => {
+    this.service.logout()
+      .then(() => {
+        this.setState({ loggedInUser: null });
+      })
+  }
+
+  fetchUser() {
+    if (this.state.loggedInUser === null) {
+      //utilizamos el método loggedin para cualquier momento que deseemos obtener la información del usuario quede guardada en el state de app
+      return this.service.loggedin()
+        .then(response => {
+          this.setState({
+            loggedInUser: response
+          })
+        })
+        .catch(err => {
+          this.setState({
+            loggedInUser: false
+          })
+        })
+    }
+  }
+
+  render() {
+    this.fetchUser()
+
+    if (this.state.loggedInUser) {
+      return (
+        <React.Fragment>
+          {/* <Redirect to="/home"></Redirect> */}
+
+          <div className="App">
+            <header className="App-header">
+              {/* <Contents></Contents> */}
+              <Switch>
+                <Route exact path='/contents' render={() => <Contents getUser={this.getUser} />} />
+              </Switch>
+            </header>
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          {/* <Redirect to="/login"></Redirect>  */}
+          <div className="App">
+            <header className="App-header">
+              <Switch>
+                <Route exact path='/signup' render={() => <Signup getUser={this.getUser} />} />
+                <Route exact path='/login' render={() => <Login getUser={this.getUser} />} />
+              </Switch>
+            </header>
+          </div>
+        </React.Fragment>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import AuthService from "./AuthService";
+import "./Login.css"
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "", logged: false };
+    this.service = new AuthService();
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const username = this.state.username;
+    const password = this.state.password;
+
+    this.service
+      .login(username, password)
+      .then(response => {
+        this.setState({
+          username: username,
+          password: password,
+          logged: true
+        });
+
+        this.props.getUser(response);
+      })
+      .catch(error => {
+        this.setState({
+          username: username,
+          password: password,
+          error: true
+        });
+      });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    if (this.state.logged) return <Redirect to={"/contents"} />;
+    return (
+      <div className="signup">
+        <div className="container">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">Future Score</h2>
+              <form
+                onSubmit={this.handleFormSubmit}
+                method="POST"
+                className="register-form"
+                id="register-form"
+              >
+                <div className="form-group">
+                  <label>
+                    <i className="zmdi zmdi-account material-icons-name" />
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={this.state.username}
+                    onChange={e => this.handleChange(e)}
+                    placeholder="Your Name"
+                  />
+                </div>
+                {/* <div className="form-group">
+                                <label><i className="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            </div> */}
+                <div className="form-group">
+                  <label>
+                    <i className="zmdi zmdi-lock" />
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="pass"
+                    value={this.state.password}
+                    onChange={e => this.handleChange(e)}
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="login"
+                    id="login"
+                    className="form-submit"
+                    value="Login"
+                  />
+                </div>
+              </form>
+              <h1> {this.state.error} ? "Error" : ""</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      // <div>
+      //   <form onSubmit={this.handleFormSubmit}>
+      //     <fieldset>
+      //       <label>Username:</label>
+      //       <input
+      //         type="text"
+      //         name="username"
+      //         value={this.state.username}
+      //         onChange={e => this.handleChange(e)}
+      //       />
+      //     </fieldset>
+
+      //     <fieldset>
+      //       <label>Password:</label>
+      //       <input
+      //         type="password"
+      //         name="password"
+      //         value={this.state.password}
+      //         onChange={e => this.handleChange(e)}
+      //       />
+      //     </fieldset>
+
+      //     <input type="submit" value="Login" />
+      //   </form>
+
+      //   <h1>{this.state.error ? "Error" : ""}</h1>
+      // </div>
+    );
+  }
+}
+
+export default Login;
