5f586db936a287af03e4619dab996be0ea5e98f4
José Luis Avilés <jlaviles@MacBook-Pro-de-Jose.local>
2019-05-09 19:09:51 +0200

    Seed created

--- server/models/Team.js
+++ server/models/Team.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const TeamSchema = new Schema({
+    teamId: Number,
     name: String,
+    tla: String,
+    crestUrl: String,
+    venue: String,
     alfa: Number,
     beta: Number,
     nu1: Number,
     nu2: Number,
     ro: Number,
-    gamma: Number,
+    gamma: Number
 });
 
-const Team = mongoose.model('Match', TeamSchema);
+const Team = mongoose.model('Team', TeamSchema);
 module.exports = Team;
