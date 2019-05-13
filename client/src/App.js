7d73d87ed6f963bc52540237d2d90696db2d866e
Ruben Polo <rpoloredondo@outlook.es>
2019-05-13 20:56:46 +0200

    competitions fixed

--- client/src/App.js
+++ client/src/App.js
@@ -62,8 +62,7 @@ class App extends Component {
           {/* <Redirect to="/home"></Redirect> */}
           <div className="App">
             <header className="App-header">
-              {/* <NavBar></NavBar> */}
-              {/* <Switch>
+<Competitions></Competitions>              {/* <Switch>
                 <Route exact path="/contents" component={Contents}> </Route> 
               </Switch> */}
             </header>
