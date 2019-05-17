401b9122d870899fc6c400d2f646d3d049954656
Ruben Polo <rpoloredondo@outlook.es>
2019-05-17 03:24:27 +0200

    Styled specific match page

--- client/src/components/contents/Competitions.js
+++ client/src/components/contents/Competitions.js
@@ -27,7 +27,7 @@ export default class Competitions extends Component {
       return (
         <div>
           <NavBar/>
-          <p className="message-user">{this.props.loggedInUser}</p>
+          <p className="message-user"></p>
           <div className="competitions">
             <h2>Competitions</h2>
               <div className="competition-container">
